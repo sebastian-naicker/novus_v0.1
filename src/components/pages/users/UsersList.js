446164d34a136d11e961a3ac80476a6a8d003447
Unknown <tdkrage@gmail.com>
2018-01-22 21:59:47 +0200

    feature: Implemented React-router & React-Router-Dom

---
+++ src/components/pages/users/UsersList.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sebastiann on 2017/06/23.
+ */
+/* eslint-disable no-unused-vars*/
+import React from 'react'
+
+const UsersList = () => (
+	<ul>
+		<li>USER 1</li>
+		<li>USER 2</li>
+		<li>USER 3</li>
+		<li>USER 4</li>
+		<li>USER 5</li>
+	</ul>
+);
+
+export default UsersList;
