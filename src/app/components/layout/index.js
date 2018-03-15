077d37350b9651673051c52a95cb58da51b2a68e
Sebastian <sebastiann@globalkinetic.com>
2018-03-15 14:54:03 +0200

    fix: removed comments

--- src/app/components/layout/index.js
+++ src/app/components/layout/index.js
@@ -1,16 +1,13 @@
-/**
- * Created by sebastiann on 2017/06/23.
- */
-
-/* eslint-disable no-unused-vars */
-
 import React from 'react';
 import lifecycle from 'utils/lifecycle';
 import Layout from './layout';
 
 const willMount = () => {
-	console.log('mounted');
+	console.log('will mount');
 };
 
-export default lifecycle()(Layout);
-// connect()(wrappedComponent);
+const didMount = () => {
+	console.log('did mount');
+};
+
+export default lifecycle({ willMount, didMount })(Layout);
