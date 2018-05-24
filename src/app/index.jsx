b9de22066e69c2517eb9a499cb61fd8fcc3c4978
Sebastian <djinnwheeler@gmail.com>
2018-05-25 00:01:07 +0200

    dev: update webpack packages and add redux packages

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function App() {
-	return (
-		<div>
-			<h1>Hello world, this is hot reload, or not</h1>
-		</div>
-	);
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello world</h1>
+			</div>
+		);
+	}
 }
